70c06af6587b4fa04b18ced92e387526422e9e9a
Radek <r.panczak@gmail.com>
2021-10-06 16:36:43 +0200

    checking Tresa

--- R/munch_ex.R
+++ R/munch_ex.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(munch)
 
+
 # Gettnau (1130) - Am 1. Januar 2021 fusionierte sie mit der Gemeinde Willisau (1151).
 
 swc_get_mun_history(1130) %>% 
@@ -24,6 +25,8 @@ t_1 <- dplyr::filter(t, mHistId.y == max(mHistId.y)) %>%
 
 t_past <- add_past(t, mutations)
 
+
+
 # no change here
 swc_get_merger_mapping_table(2015, 2018, type = "compact") %>% 
   filter(mun_id_x == 1130)
@@ -33,4 +36,10 @@ swc_get_merger_mapping_table(2015, 2021, type = "compact") %>%
   filter(mun_id_x == 1130)
 
 
+# Tresa
+swc_get_merger_mapping_table(2015, 2018, type = "compact") %>% 
+  filter(mun_id_x == 5239)
+
+swc_get_merger_mapping_table(2015, 2021, type = "compact") %>% 
+  filter(mun_id_x %in% c(5178, 5202, 5213, 5222))
 

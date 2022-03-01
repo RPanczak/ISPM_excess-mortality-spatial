b89d4d5b7144a94c879301790048273f2f9d5453
Radek <r.panczak@gmail.com>
2022-03-01 17:42:57 +0100

    more work on netCDF

--- R/nc_meteoswiss.R
+++ R/nc_meteoswiss.R
@@ -2,12 +2,31 @@
 library(sf)
 library(ncdf4)
 library(raster)
-library(tidyverse)
 
 # #############################
-# nc
+# via RNetCDF
 
-TabsD_14 <- nc_open('data-raw/meteoswiss/TabsD_ch01r.swiss.lv95_201401010000_201412310000.nc')
+# library(RNetCDF)
+# TabsD_14 <- open.nc(filename)
+# print.nc(TabsD_14)
+# 
+# TabsD_14 <- read.nc(open.nc(filename))
+# 
+# TabsD_14$E
+# TabsD_14$TabsD
+# 
+# att.get.nc(open.nc(filename), "time", "units")
+
+# #############################
+# via ncdf4
+
+filename <- "data-raw/meteoswiss/TabsD_ch01r.swiss.lv95_201401010000_201412310000.nc"
+
+TabsD_14 <- nc_open(filename)
+
+# library(ncmeta)
+# nc_inq(filename)     ## one-row summary of file
+# nc_dims(filename)    ## all dimensions
 
 E <- ncvar_get(TabsD_14, "E") # X, lon
 N <- ncvar_get(TabsD_14, "N") # Y, lat
@@ -15,7 +34,8 @@ t <- ncvar_get(TabsD_14, "time")
 
 timestamp <- lubridate::as_date(t, origin = "1900-01-01")
 
-data <- ncvar_get(TabsD_14, "TabsD") # store the data in a 3-dimensional array
+# store the data in a 3-dimensional array
+data <- ncvar_get(TabsD_14, "TabsD") 
 dim(data) 
 
 # NAs?
@@ -26,6 +46,17 @@ fillvalue
 plot(data.frame(date = timestamp,
                 temp = data[100, 100, ]),
      type = "l")
+
+# #############################
+# two example points to extract 
+data[100, 100, ]
+data[200, 200, ]
+
+ex0 <- data.frame(rbind(data[100, 100, ],
+                        data[200, 200, ]))
+
+pt <- cbind(c(E[100], E[200]), 
+            c(N[100], N[200]))
 
 # #############################
 # raster solution 
@@ -46,16 +77,50 @@ r <- raster::raster(data_slice,
                     ymn = min(N), ymx = max(N), 
                     crs = st_crs(2056)$proj4string)
 
-
-plot(r, main = "TabsD_14",
+plot(r, main = "TabsD_14 - 1st January 2014",
      col = rev(RColorBrewer::brewer.pal(11, "RdBu")))
+
+# extract by point
+ex1 <- data.frame("X2014.12.27" = raster::extract(r, pt))
 
 # #############################
 # brick solution 
 
-b <- brick("data-raw/meteoswiss/TabsD_ch01r.swiss.lv95_201401010000_201412310000.nc",
-           crs = st_crs(2056)$proj4string)
+# 12 months combined 
+b <- brick(filename)
+# raster::crs(b) <- "EPSG:2056"
 
-plot(b[[1]])
+nlayers(b)
 
-spplot(stack(b[[1]], b[[7]]))
+spplot(b[[1]])
+
+spplot(stack(b[[1]], b[[182]]))
+
+# extract by point
+ex2 <- data.frame(raster::extract(b, pt))
+
+# #############################
+# terra solution
+
+terra <- terra::rast(filename)
+crs(terra) <- "epsg:2056"
+
+# summary(data)
+plot(terra[[ c(1, 182) ]], range = c(-20, 25))
+
+time(terra)
+
+# extract by point
+ex3 <- data.frame(terra::extract(terra, pt))
+
+# #############################
+# results
+
+# one month
+ex0$X1
+ex1
+
+# 12 months
+ex0
+ex2
+ex3
--- R/nc_meteoswiss_open.R
+++ R/nc_meteoswiss_open.R
@@ -2,37 +2,47 @@
 library(sf)
 library(ncdf4)
 library(raster)
-library(tidyverse)
 
 # #############################
 # data 
 
-download.file(url = "https://www.meteoswiss.admin.ch/content/dam/meteoswiss/de/Ungebundene-Seiten/Produkte/doc/tnorm9120.zip",
-              destfile = "data-raw/tnorm9120.zip")
-unzip("data-raw/tnorm9120.zip", exdir = "data-raw")
-unlink("data-raw/tnorm9120.zip")
+# download.file(url = "https://www.meteoswiss.admin.ch/content/dam/meteoswiss/de/Ungebundene-Seiten/Produkte/doc/tnorm9120.zip",
+#               destfile = "data-raw/tnorm9120.zip")
+# unzip("data-raw/tnorm9120.zip", exdir = "data-raw")
+# unlink("data-raw/tnorm9120.zip")
+
+filename <- "data-raw/tnorm9120/TnormM9120_ch01r.swiss.lv95_000001010000_000012010000.nc"
 
 # #############################
-# nc
+# via ncdf4
 
-tnorm9120 <- nc_open("data-raw/tnorm9120/TnormM9120_ch01r.swiss.lv95_000001010000_000012010000.nc")
+tnorm9120 <- nc_open(filename)
 tnorm9120
 
+# EPSG 2056 coordinate system
 E <- ncvar_get(tnorm9120, "E") # X, lon
 N <- ncvar_get(tnorm9120, "N") # Y, lat
 t <- ncvar_get(tnorm9120, "time")
 
-data <- ncvar_get(tnorm9120, "TnormM9120") # store the data in a 3-dimensional array
+# store the data in a 3-dimensional array
+data <- ncvar_get(tnorm9120, "TnormM9120")
 dim(data) 
-
-# NAs?
-fillvalue <- ncatt_get(tnorm9120, "TnormM9120", "_FillValue")
-fillvalue
 
 # one pixel example 
 plot(data.frame(date = t,
                 temp = data[100, 100, ]),
      type = "l")
+
+# #############################
+# two example points to extract 
+data[100, 100, ]
+data[200, 200, ]
+
+ex0 <- data.frame(rbind(data[100, 100, ],
+                        data[200, 200, ]))
+
+pt <- cbind(c(E[100], E[200]), 
+            c(N[100], N[200]))
 
 # #############################
 # raster solution 
@@ -44,26 +54,60 @@ data_slice <- data[, , 1]
 data_slice <- apply(t(data_slice), 2, rev)
 dim(data_slice) 
 
-# st_crs(2056)$proj4string
-
 r <- raster::raster(data_slice,
                     xmn = min(E), xmx = max(E), 
                     ymn = min(N), ymx = max(N), 
                     crs = st_crs(2056)$proj4string)
 
-
 plot(r, main = "TnormM9120 - January",
      col = rev(RColorBrewer::brewer.pal(11, "RdBu")))
+
+# extract by point
+ex1 <- data.frame("X2014.12.27" = raster::extract(r, pt))
 
 # #############################
 # brick solution 
 
-b <- brick("data-raw/tnorm9120/TnormM9120_ch01r.swiss.lv95_000001010000_000012010000.nc",
-           crs = st_crs(2056)$proj4string)
+# 12 months combined 
+b <- brick(filename,
+           varname = "TnormM9120", 
+           crs = paste(st_crs(2056)$proj4string))
 
-# Jamuary
-plot(b[[1]])
+nlayers(b)
+
+# January
+spplot(b[[1]])
 
 # Jan vs Jul
 spplot(stack(b[[1]], b[[7]]))
 
+# extract by point
+ex2 <- data.frame(raster::extract(b, pt))
+
+# #############################
+# terra solution
+
+terra <- terra::rast(filename)
+crs(terra) <- "epsg:2056"
+
+# summary(data)
+plot(terra, range = c(-20, 25))
+plot(terra[[ c(1, 7) ]], range = c(-20, 25))
+
+# extract by point
+ex3 <- data.frame(terra::extract(terra, pt))
+
+# t(ex3)
+# seq(from = as.Date("2014-01-01"), to = as.Date("2014-12-31"), by = "day")
+
+# #############################
+# results
+
+# one month
+ex0$X1
+ex1
+
+# 12 months
+ex0
+ex2
+ex3

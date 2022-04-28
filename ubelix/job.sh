9d2954e76a34ccebda8a61e2752168436e2ca53c
Radoslaw Panczak <31648642+RPanczak@users.noreply.github.com>
2022-04-28 18:33:49 +0200

    init of hpc transition

---
+++ ubelix/job.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --job-name="INLA AR iid"
+#SBATCH --cpus-per-task=32
+#SBATCH --mem-per-cpu=2G
+#SBATCH --time=48:00:00
+#SBATCH --mail-user=r.panczak@gmail.com
+#SBATCH --mail-type=end,fail
+
+module load R 
+Rscript analyses/06-01_model-ar.R

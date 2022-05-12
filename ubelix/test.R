2b3e43e9f26558990ae9dafbddb838e23cd14317
Radoslaw Panczak <31648642+RPanczak@users.noreply.github.com>
2022-05-12 15:22:42 +0200

    print some stuff

--- ubelix/test.R
+++ ubelix/test.R
@@ -12,7 +12,10 @@ y = rnorm(n, mean = eta, sd = 1/sqrt(prec))
 
 
 data = list(y=y, z=z)
+data
 formula = y ~ 1+z
+formula
+
 result = inla(formula, family = "gaussian", data = data)
 
 summary(result)

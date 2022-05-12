333e7ddab3aa056f5ba6ca9a83e4cbd7092ca10f
Radoslaw Panczak <31648642+RPanczak@users.noreply.github.com>
2022-05-12 14:56:29 +0200

    test job

---
+++ ubelix/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --job-name="INLA test"
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=4G
+#SBATCH --time=00:30:00
+#SBATCH --mail-user=radoslaw.panczak@ispm.unibe.ch
+#SBATCH --mail-type=end,fail
+
+module load R 
+Rscript ~/ISPM_geo-mortality/ubelix/test.R

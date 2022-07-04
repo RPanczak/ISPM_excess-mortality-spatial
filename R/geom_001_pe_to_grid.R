7515a1306a70392583bc968390c9fea6bc6d1f83
Radoslaw Panczak <31648642+RPanczak@users.noreply.github.com>
2022-07-04 20:26:08 +0200

    rework to bypass stata

--- R/geom_001_pe_to_grid.R
+++ R/geom_001_pe_to_grid.R
@@ -1,19 +1,40 @@
 # Extract population by grid/gemeinde from STATPOP
-# Radek and Julien, 2022-03-08
+# Radek and Julien, 2022-07-04
 
 geom_001_pe_to_grid = function(path) {
-  r_pe <- read_dta(path) %>% 
-    zap_label() %>% zap_formats() %>% 
-    # check sex coding since it seems to be different than SNC?
+  r_pe <- read_delim(path, 
+                     delim = ";", escape_double = FALSE, 
+                     col_types = cols(STATYEAR = col_integer(), 
+                                      SEX = col_integer(), 
+                                      TYPEOFRESIDENCE = col_integer(), 
+                                      POPULATIONTYPE = col_integer(), 
+                                      AGE = col_integer(), 
+                                      CLASSAGEFIVEYEARS = col_integer(), 
+                                      NATIONALITYCATEGORY = col_integer(), 
+                                      MAINRESIDENCECATEGORY = col_integer(), 
+                                      GEOCOORDE = col_integer(), 
+                                      GEOCOORDN = col_integer(), 
+                                      INDIC_EGID = col_integer(), 
+                                      statdate = col_date(format = "%d/%m/%Y")), 
+                     trim_ws = TRUE) %>% 
+    janitor::remove_empty(c("rows", "cols")) %>% janitor::clean_names() %>% 
+    rename(egid = federalbuildingid) %>% 
+    filter(typeofresidence == 1) %>% 
+    filter(populationtype == 1) %>% 
+    filter(mainresidencecategory == 1) %>% 
+    filter(indic_egid == 1) %>% 
+    dplyr::select(-statyear, -statdate, 
+                  -typeofresidence, -populationtype, -mainresidencecategory,
+                  -indic_egid) %>% 
     mutate(
       sex = if_else(sex == "2", "Female", "Male"),
       sex = factor(sex),
       age = cut(age, 
                 breaks = c(0, 40, seq(50, 80, 10), 120), right = FALSE, 
                 labels = c("<40", "40-49", "50-59", "60-69", "70-79", "80+"))) %>% 
-    group_by(year, geocoorde, geocoordn, sex, age) %>% 
+    group_by(egid, geocoorde, geocoordn) %>% 
     summarise(n = n()) %>% 
-    ungroup()  %>% 
+    ungroup() %>% 
     st_as_sf(coords = c("geocoorde", "geocoordn"), 
              crs = 2056,
              remove = TRUE) %>% 

165fa555d720e21649d775f74a19e2afc2e73c40
Radoslaw Panczak <31648642+RPanczak@users.noreply.github.com>
2022-07-05 09:47:43 +0200

    new INLA set up

--- ubelix/job.sh
+++ ubelix/job.sh
@@ -6,5 +6,7 @@
 #SBATCH --mail-user=radoslaw.panczak@ispm.unibe.ch
 #SBATCH --mail-type=end,fail
 
-module load R 
+ml vital-it
+ml R/3.6.1
+
 Rscript ~/ISPM_geo-mortality/analyses/06-01_model-ar.R
--- ubelix/test.sh
+++ ubelix/test.sh
@@ -6,5 +6,7 @@
 #SBATCH --mail-user=radoslaw.panczak@ispm.unibe.ch
 #SBATCH --mail-type=end,fail
 
-module load GCC R 
+ml vital-it
+ml R/3.6.1
+ 
 Rscript ~/ISPM_geo-mortality/ubelix/test.R

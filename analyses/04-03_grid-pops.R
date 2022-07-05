96d7cc9f418585a393865078a2ed62f0a2f50a6c
Radoslaw Panczak <31648642+RPanczak@users.noreply.github.com>
2022-07-05 19:15:41 +0200

    lme4 draft of pops modelling

---
+++ analyses/04-03_grid-pops.R
@@ -0,0 +1,47 @@
+# ###############################
+# Extrapolating pops
+# ML solution
+
+library(readr)
+library(dplyr)
+library(tidyr)
+library(tibble)
+
+# ###############################
+# pop by grid
+st_grid_pop_pred <- read_rds("data/blob/st_grid_pop_exp.Rds")
+
+# ###############################
+# GLM
+m1 <- glm(pop ~ age + sex + year_month,
+          data = st_grid_pop_pred,
+          family = poisson(link = "log"))
+
+st_grid_pop_pred$m1 <- as.integer(predict(m1, 
+                                          st_grid_pop_pred, 
+                                          type = "response"))
+
+# ###############################
+# GLMER 1
+library(lme4)
+
+m2 <- glmer(pop ~ age + sex + year_month + (1 | ID),
+            data = st_grid_pop_pred,
+            family = poisson(link = "log"))
+
+st_grid_pop_pred$m2 <- as.integer(predict(m2, 
+                                          st_grid_pop_pred, 
+                                          type = "response"))
+
+# ###############################
+# GLMER 2
+m3 <- glmer(pop ~ age + sex + year_month + (year_month | ID),
+            data = st_grid_pop_pred,
+            family = poisson(link = "log")
+)
+
+st_grid_pop_pred$m3 <- as.integer(predict(m3, 
+                                          st_grid_pop_pred, 
+                                          type = "response"))
+
+write_rds(st_grid_pop_pred, file = "data/blob/st_grid_pop_pred.Rds")

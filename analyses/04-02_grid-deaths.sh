1b820019b2af3d65e10e2e04d7431f9ea6562ff9
Radoslaw Panczak <31648642+RPanczak@users.noreply.github.com>
2022-07-06 17:10:50 +0200

    bump up time request
    24h was not sufficient

--- analyses/04-02_grid-deaths.sh
+++ analyses/04-02_grid-deaths.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name="gr-de1"
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=16G
-#SBATCH --time=24:00:00
+#SBATCH --mem-per-cpu=8G
+#SBATCH --time=168:00:00
 #SBATCH --mail-user=radoslaw.panczak@ispm.unibe.ch
 #SBATCH --mail-type=end,fail
 

fb2c26dd2bfe48aa94dbd95da787079cf37ab4e3
Radoslaw Panczak <31648642+RPanczak@users.noreply.github.com>
2022-07-06 09:47:31 +0200

    glm out, save models

--- analyses/04-03_grid-pops.R
+++ analyses/04-03_grid-pops.R
@@ -1,6 +1,6 @@
 # ###############################
 # Extrapolating pops
-# ML solution
+# simple, multilevel solution
 
 library(readr)
 library(dplyr)
@@ -10,16 +10,6 @@ library(tibble)
 # ###############################
 # pop by grid
 st_grid_pop_pred <- read_rds("data/blob/st_grid_pop_exp.Rds")
-
-# ###############################
-# GLM
-# m1 <- glm(pop ~ age + sex + year,
-#           data = st_grid_pop_pred,
-#           family = poisson(link = "log"))
-# 
-# st_grid_pop_pred$m1 <- as.integer(predict(m1, 
-#                                           st_grid_pop_pred, 
-#                                           type = "response"))
 
 # ###############################
 # GLMER 1
@@ -40,8 +30,11 @@ m3 <- glmer(pop ~ age + sex + year + (year | ID),
             family = poisson(link = "log")
 )
 
-st_grid_pop_pred$m3 <- as.integer(predict(m3, 
-                                          st_grid_pop_pred, 
+st_grid_pop_pred$m3 <- as.integer(predict(m3,
+                                          st_grid_pop_pred,
                                           type = "response"))
+
+write_rds(m2, file = "data/blob/m2.Rds")
+write_rds(m3, file = "data/blob/m3.Rds")
 
 write_rds(st_grid_pop_pred, file = "data/blob/st_grid_pop_pred.Rds")

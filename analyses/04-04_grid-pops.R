a2ff0cd4f89575023a6a97d0da591006cfee6654
Radoslaw Panczak <31648642+RPanczak@users.noreply.github.com>
2022-07-07 15:43:54 +0200

    testing zip0

--- analyses/04-04_grid-pops.R
+++ analyses/04-04_grid-pops.R
@@ -13,83 +13,49 @@ library(INLA)
 
 # ###############################
 # pop by grid
-st_grid_pop_pred <- read_rds("data/blob/st_grid_pop_exp.Rds")
+st_grid_pop_exp <- read_rds("data/blob/st_grid_pop_exp.Rds")
 
 # sample dataset for testing
-# st_grid_pop_pred <- st_grid_pop_pred %>% 
-#   mutate(ID = factor(ID)) %>% 
-#   filter(ID %in% sample(levels(ID), 10))
+st_grid_pop_exp <- st_grid_pop_exp %>%
+  mutate(ID = factor(ID)) %>%
+  filter(ID %in% sample(levels(ID), 100)) %>% 
+  mutate(ID = forcats::fct_drop(ID))
+
+# length(unique(st_grid_pop_exp$ID))
 
 # ###############################
-# INLA
+# setup
 
 hyper.iid <- list(theta = list(prior = "pc.prec", param = c(1, 0.01)))
-
-# hyper.bym <- list(theta1 = list("PCprior", c(1, 0.01)), 
-#                   theta2 = list("PCprior", c(0.5, 0.5)))
 
 control.family <- inla.set.control.family.default()
 threads <- parallel::detectCores()
 
-# Stratified
+# family = "Poisson"
+family = "zeroinflatedpoisson0"
+# family = "zeroinflatedpoisson1"
+# family = "zeroinflatednbinomial0"
+# family = "zeroinflatednbinomial1"
 
-data_stratified <- st_grid_pop_pred %>%
-  filter(age == "<40", sex == "Female")
+# ###############################
+# models
 
-f1 <- pop ~ 
-  # year +
-  f(id_year, model = "iid", hyper = hyper.iid, constr = TRUE) +
-  f(ID, model = "iid", hyper = hyper.iid, constr = TRUE)
-
-m1 <- inla(f1,
-                data = data_stratified,
-                family = "Poisson",
-                # family = "zeroinflatedpoisson0",
-                # family = "zeroinflatedpoisson1",
-                # family = "zeroinflatednbinomial0",
-                # family = "zeroinflatednbinomial1",
-                # verbose = TRUE,
-                control.family = control.family,
-                control.compute = list(
-                  config = TRUE,
-                  # return.marginals.predictor = TRUE,
-                  # cpo = TRUE,
-                  dic = TRUE, waic = TRUE
-                ),
-                control.mode = list(restart = TRUE),
-                num.threads = threads,
-                control.predictor = list(compute = TRUE, link = 1),
-                control.inla = list(
-                  strategy = "simplified.laplace", # default
-                  # strategy = "adaptive",
-                  # strategy = "gaussian",
-                  # strategy = "laplace", #npoints = 21,
-                  int.strategy = "ccd" # default
-                  # int.strategy = "grid", diff.logdens = 4
-                )
-)
-
-# Adjusted
+# Adjusted 1
 
 f2 <- pop ~ 
   sex + age +
   f(id_year, model = "iid", hyper = hyper.iid, constr = TRUE) +
-  # f(ID, model = "bym2", graph = "data/nb/ar21_wm_q.adj", scale.model = TRUE, constr = TRUE, hyper = hyper.bym)
   f(ID, model = "iid", constr = TRUE, hyper = hyper.iid)
 
-m2 <- inla(f2,
-                data = st_grid_pop_pred,
-                family = "Poisson",
-                # family = "zeroinflatedpoisson0",
-                # family = "zeroinflatedpoisson1",
-                # family = "zeroinflatednbinomial0",
-                # family = "zeroinflatednbinomial1",
-                # verbose = TRUE,
+m2_zip0 <- inla(f2,
+                data = st_grid_pop_exp,
+                family = family,
+                verbose = TRUE,
                 control.family = control.family,
                 control.compute = list(
                   config = TRUE,
                   # return.marginals.predictor = TRUE,
-                  # cpo = TRUE,
+                  cpo = TRUE,
                   dic = TRUE, waic = TRUE
                 ),
                 control.mode = list(restart = TRUE),
@@ -104,27 +70,26 @@ m2 <- inla(f2,
                   # int.strategy = "grid", diff.logdens = 4
                 )
 )
+
+summary(m2_zip0)
+
+# Adjusted 2
 
 f3 <- pop ~ 
   sex +
   f(id_age, model = "iid", hyper = hyper.iid, constr = TRUE) +
   f(id_year, model = "iid", hyper = hyper.iid, constr = TRUE) +
-  # f(ID, model = "bym2", graph = "data/nb/ar21_wm_q.adj", scale.model = TRUE, constr = TRUE, hyper = hyper.bym)
   f(ID, model = "iid", constr = TRUE, hyper = hyper.iid)
 
-m3 <- inla(f3,
-                data = st_grid_pop_pred,
-                family = "Poisson",
-                # family = "zeroinflatedpoisson0",
-                # family = "zeroinflatedpoisson1",
-                # family = "zeroinflatednbinomial0",
-                # family = "zeroinflatednbinomial1",
-                # verbose = TRUE,
+m3_zip0 <- inla(f3,
+                data = st_grid_pop_exp,
+                family = family,
+                verbose = TRUE,
                 control.family = control.family,
                 control.compute = list(
                   config = TRUE,
                   # return.marginals.predictor = TRUE,
-                  # cpo = TRUE,
+                  cpo = TRUE,
                   dic = TRUE, waic = TRUE
                 ),
                 control.mode = list(restart = TRUE),
@@ -140,8 +105,8 @@ m3 <- inla(f3,
                 )
 )
 
-write_rds(m1, file = "data/blob/m1.Rds")
-write_rds(m2, file = "data/blob/m2.Rds")
-write_rds(m3, file = "data/blob/m3.Rds")
+summary(m3_zip0)
 
-# write_rds(st_grid_pop_pred, file = "data/blob/st_grid_pop_pred.Rds")
+write_rds(m1_zip0, "data/blob/m1_zip0.Rds")
+write_rds(m2_zip0, "data/blob/m2_zip0.Rds")
+write_rds(m3_zip0, "data/blob/m3_zip0.Rds")

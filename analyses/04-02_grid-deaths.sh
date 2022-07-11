be14bf06fb1ab2a6ae83d3d42c6f639a9eb878f6
Radoslaw Panczak <31648642+RPanczak@users.noreply.github.com>
2022-07-11 08:51:19 +0200

    revert

--- analyses/04-02_grid-deaths.sh
+++ analyses/04-02_grid-deaths.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name="gr-de1"
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=8G
-#SBATCH --time=14-00:00:00
+#SBATCH --time=96:00:00
 #SBATCH --mail-user=radoslaw.panczak@ispm.unibe.ch
 #SBATCH --mail-type=end,fail
 
